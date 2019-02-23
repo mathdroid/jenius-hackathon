c76bf583c6f8857e52226e57a5043fe2cfc8c4e2
Resi Respati <resir014@gmail.com>
2019-02-23 09:59:45 +0700

    styled-components

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <RootView>
         <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      </RootView>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const RootView = styled(View)`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`
