4aa5757081fbbdb012b2bbced9ece1a4f6d22b61
Resi Respati <resir014@gmail.com>
2019-02-24 03:51:13 +0700

    input expenses

--- mobile-app/pages/finance/budget/index.js
+++ mobile-app/pages/finance/budget/index.js
@@ -83,64 +83,126 @@ const FloatingButtonHolder = styled(TouchableOpacity)`
   box-shadow: 1px 2px 8px rgba(0, 0, 0, 0.25);
 `;
 
-export default function FinanceBudgetScreen() {
-  return (
-    <FontLoader
-      fonts={{
-        'inter-bold': require('../../../assets/fonts/Inter-Bold.ttf'),
-        'inter-regular': require('../../../assets/fonts/Inter-Regular.ttf'),
-        'inter-extralight': require('../../../assets/fonts/Inter-ExtraLight-BETA.ttf'),
-      }}
-    >
-      <RootView>
-        <FloatingButtonHolder onPress={() => null}>
-          <Icon name="md-add" color="#FFF" size={32} />
-        </FloatingButtonHolder>
-        <BudgetCard colors={['#2234ce', '#00d28a']} start={[0, 0.5]} end={[1, 0.5]}>
-          <BudgetCardGrid>
-            <MonthPickerToggle>
-              <Icon name="md-more" color="#FFF" size={32} />
-            </MonthPickerToggle>
-            <BudgetCardMonthText bold>Maret 2019</BudgetCardMonthText>
-            <StyledText>Untuk Dianggarkan</StyledText>
-            <BudgetCardPrimaryText bold larger>
-              9,000,000
-            </BudgetCardPrimaryText>
-          </BudgetCardGrid>
-          <BudgetCardGrid last>
-            <BudgetCardGridInner>
-              <StyledText>Pengeluaran Lebih Bulan Lalu</StyledText>
-              <BudgetCardPrimaryText bold>225,000</BudgetCardPrimaryText>
-            </BudgetCardGridInner>
-            <BudgetCardGridInner last>
-              <StyledText>Uang Lebih Untuk Bulan Depan</StyledText>
-              <BudgetCardPrimaryText bold>1,355,000</BudgetCardPrimaryText>
-            </BudgetCardGridInner>
-          </BudgetCardGrid>
-        </BudgetCard>
-        <BudgetList
-          ListHeaderComponent={<ListHeader color="magenta" title="Pengeluaran" />}
-          data={[
-            { key: 'Makanan', budgeted: 2000000, spent: 0, balance: 2000000 },
-            { key: 'General', budgeted: 500000, spent: 0, balance: 1500000 },
-            { key: 'Tagihan', budgeted: 400000, spent: 0, balance: 1000000 },
-            { key: 'Tagihan Fleksibel', budgeted: 0, spent: 0, balance: 0 },
-          ]}
-          renderItem={({ item }) => <ListRow item={item} />}
-        />
-        <BudgetList
-          ListHeaderComponent={
-            <ListHeader isIncome color="#9b45ff" title="Simpanan &amp; Investasi" />
-          }
-          data={[{ key: 'Deposito', income: 2000000 }, { key: 'Reksadana', income: 1500000 }]}
-          renderItem={({ item }) => <ListRow isIncome item={item} />}
-        />
-        <BudgetList
-          ListHeaderComponent={<ListHeader isIncome color="#00d28a" title="Pendapatan" />}
-          data={[{ key: 'Gaji', income: 2000000 }, { key: 'Hasil Usaha', income: 2000000 }]}
-          renderItem={({ item }) => <ListRow isIncome item={item} />}
-        />
-      </RootView>
-    </FontLoader>
-  );
+export default class FinanceBudgetScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      available: {
+        food: 2000000,
+        general: 500000,
+        bills: 400000,
+        flexible: 0,
+      },
+      spent: {
+        food: 0,
+        general: 0,
+        bills: 0,
+        flexible: 0,
+      },
+    };
+  }
+
+  render() {
+    const { available, spent } = this.state;
+
+    return (
+      <FontLoader
+        fonts={{
+          'inter-bold': require('../../../assets/fonts/Inter-Bold.ttf'),
+          'inter-regular': require('../../../assets/fonts/Inter-Regular.ttf'),
+          'inter-extralight': require('../../../assets/fonts/Inter-ExtraLight-BETA.ttf'),
+        }}
+      >
+        <RootView>
+          <FloatingButtonHolder onPress={() => null}>
+            <Icon name="md-add" color="#FFF" size={32} />
+          </FloatingButtonHolder>
+          <BudgetCard colors={['#2234ce', '#00d28a']} start={[0, 0.5]} end={[1, 0.5]}>
+            <BudgetCardGrid>
+              <MonthPickerToggle>
+                <Icon name="md-more" color="#FFF" size={32} />
+              </MonthPickerToggle>
+              <BudgetCardMonthText bold>Maret 2019</BudgetCardMonthText>
+              <StyledText>Untuk Dianggarkan</StyledText>
+              <BudgetCardPrimaryText bold larger>
+                9,000,000
+              </BudgetCardPrimaryText>
+            </BudgetCardGrid>
+            <BudgetCardGrid last>
+              <BudgetCardGridInner>
+                <StyledText>Pengeluaran Lebih Bulan Lalu</StyledText>
+                <BudgetCardPrimaryText bold>225,000</BudgetCardPrimaryText>
+              </BudgetCardGridInner>
+              <BudgetCardGridInner last>
+                <StyledText>Uang Lebih Untuk Bulan Depan</StyledText>
+                <BudgetCardPrimaryText bold>1,355,000</BudgetCardPrimaryText>
+              </BudgetCardGridInner>
+            </BudgetCardGrid>
+          </BudgetCard>
+          <BudgetList
+            ListHeaderComponent={<ListHeader color="magenta" title="Pengeluaran" />}
+            data={[
+              {
+                key: 'food',
+                label: 'Makanan',
+                budgeted: available.food,
+                spent: spent.food,
+                balance: available.food - spent.food,
+              },
+              {
+                key: 'general',
+                label: 'General',
+                budgeted: available.general,
+                spent: spent.general,
+                balance: available.general - spent.general,
+              },
+              {
+                key: 'bills',
+                label: 'Tagihan',
+                budgeted: available.bills,
+                spent: spent.bills,
+                balance: available.bills - spent.bills,
+              },
+              {
+                key: 'flexible',
+                label: 'Tagihan Fleksibel',
+                budgeted: available.flexible,
+                spent: spent.flexible,
+                balance: available.flexible - spent.flexible,
+              },
+            ]}
+            keyExtractor={item => item.key || item.label}
+            renderItem={({ item }) => (
+              <ListRow
+                item={item}
+                onRowChange={(value, key) =>
+                  this.setState(prevState => ({
+                    spent: {
+                      ...prevState.spent,
+                      [key]: value,
+                    },
+                  }))
+                }
+              />
+            )}
+          />
+          <BudgetList
+            ListHeaderComponent={
+              <ListHeader isIncome color="#9b45ff" title="Simpanan &amp; Investasi" />
+            }
+            data={[{ label: 'Deposito', income: 2000000 }, { label: 'Reksadana', income: 1500000 }]}
+            keyExtractor={item => item.key || item.label}
+            renderItem={({ item }) => <ListRow isIncome item={item} />}
+          />
+          <BudgetList
+            ListHeaderComponent={<ListHeader isIncome color="#00d28a" title="Pendapatan" />}
+            data={[{ label: 'Gaji', income: 2000000 }, { label: 'Hasil Usaha', income: 2000000 }]}
+            keyExtractor={item => item.key || item.label}
+            renderItem={({ item }) => <ListRow isIncome item={item} />}
+          />
+        </RootView>
+      </FontLoader>
+    );
+  }
 }
