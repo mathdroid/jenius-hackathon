b46b833a633b6f8716f38b6569370b3a3e4f084a
Resi Respati <resir014@gmail.com>
2019-02-23 23:36:45 +0700

    link finance

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,12 +1,16 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import HomeScreen from './pages/home';
+import FinanceScreen from './pages/finance';
 import BandersnatchScreen from './pages/bandersnatch';
 
 const AppNavigator = createStackNavigator(
   {
     Home: {
       screen: HomeScreen,
+    },
+    Finance: {
+      screen: FinanceScreen,
     },
     Bandersnatch: {
       screen: BandersnatchScreen,
